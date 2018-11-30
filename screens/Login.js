17cda0cbad463ca5d517bb0db440d4070da470a5
cleyra uzcategui <cleyrauzcategui@unknown-88:e9:fe:63:8c:e4.lan>
2018-11-30 11:21:25 +0000

    Added icons, utils and gradient buttons

--- screens/Login.js
+++ screens/Login.js
@@ -8,6 +8,8 @@ import {
   TouchableHighlight,
   View,
   Alert,
+  Dimensions,
+  Keyboard,
   TextInput,
   AsyncStorage,
 } from 'react-native';
@@ -16,7 +18,13 @@ import {
   RkTextInput,
   RkText,
   RkCard,
+  RkAvoidKeyboard,
+  RkStyleSheet,
+  RkTheme,
 } from 'react-native-ui-kitten';
+import { FontAwesome } from '../assets/icons';
+import { GradientButton } from '../components/gradientButton';
+import { scaleModerate, scaleVertical } from '../utils/scale';
 
 export default class Login extends React.Component {
   static navigationOptions = {
@@ -30,6 +38,35 @@ export default class Login extends React.Component {
       password: ' '
     };
   };
+
+getThemeImageSource = (theme) => (
+  theme.name === 'light' ?
+    require('../../assets/images/backgroundLoginV1.png') : require('../../assets/images/backgroundLoginV1DarkTheme.png')
+);
+
+renderImage = () => {
+  const screenSize = Dimensions.get('window');
+  const imageSize = {
+    width: screenSize.width,
+    height: screenSize.height - scaleModerate(375, 1),
+  };
+  return (
+      <Image
+        style={[styles.image, imageSize]}
+        source={this.getThemeImageSource(RkTheme.current)}
+      />
+    );
+  };
+
+  onLoginButtonPressed = () => {
+    this.props.navigation.goBack();
+  };
+
+  onSignUpButtonPressed = () => {
+    this.props.navigation.navigate('SignUp');
+  };
+
+  //
 
   cancelLogin = ()=>{
     Alert.alert('Login cancelled!');
@@ -127,6 +164,45 @@ export default class Login extends React.Component {
       </RkCard>
     );
   }
+}
+
+render = () => (
+  <RkAvoidKeyboard
+    onStartShouldSetResponder={() => true}
+    onResponderRelease={() => Keyboard.dismiss()}
+    style={styles.screen}>
+    {this.renderImage()}
+    <View style={styles.container}>
+      <View style={styles.buttons}>
+        <RkButton style={styles.button} rkType='social'>
+          <RkText rkType='awesome hero accentColor'>{FontAwesome.twitter}</RkText>
+        </RkButton>
+        <RkButton style={styles.button} rkType='social'>
+          <RkText rkType='awesome hero accentColor'>{FontAwesome.google}</RkText>
+        </RkButton>
+        <RkButton style={styles.button} rkType='social'>
+          <RkText rkType='awesome hero accentColor'>{FontAwesome.facebook}</RkText>
+        </RkButton>
+      </View>
+      <RkTextInput rkType='rounded' placeholder='Username' />
+      <RkTextInput rkType='rounded' placeholder='Password' secureTextEntry />
+      <GradientButton
+        style={styles.save}
+        rkType='large'
+        onPress={this.onLoginButtonPressed}
+        text='LOGIN'
+      />
+      <View style={styles.footer}>
+        <View style={styles.textRow}>
+          <RkText rkType='primary3'>Don’t have an account?</RkText>
+          <RkButton rkType='clear'>
+            <RkText rkType='header6' onPress={this.onSignUpButtonPressed}>Sign up now</RkText>
+          </RkButton>
+        </View>
+      </View>
+    </View>
+  </RkAvoidKeyboard>
+)
 }
 
 const styles = StyleSheet.create({
